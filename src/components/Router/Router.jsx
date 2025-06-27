788daa57a52ecf466fc8e8f5e6624be351166d10
JoelleTP <joe.pflieg@hotmail.fr>
2025-06-27 14:22:15 +0200

    Initialization of the Vite application, its structure, and dependencies”

---
+++ src/components/Router/Router.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+
+function Router() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Router
