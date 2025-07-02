943a7b2745659b4303272c3ea113841f2126f2ef
JoelleTP <joe.pflieg@hotmail.fr>
2025-07-02 14:51:42 +0200

    Integration of the login page on React

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Features from "../components/Features/Features.jsx"
 
 function Home() {
     return (
-        <>
+        <main>
             <div className="hero">
                 <section className="hero-content">
                     <h2 className="sr-only">Promoted Content</h2>
@@ -24,7 +24,7 @@ function Home() {
                     />
                 ))}
             </section>      
-        </>
+        </main>
     )
 }
 
